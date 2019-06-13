bd4aa99ae081736c2f1ba93f834a7a99936736ea
Gaston Bia Palombo <gastibia@gmail.com>
2019-06-13 20:38:41 -0300

    a

--- src/grafo/GrafoDinamic.java
+++ src/grafo/GrafoDinamic.java
@@ -164,33 +164,35 @@ public class GrafoDinamic implements GrafosTDA {
 		return null;
 	}
 	
-	public void DFS(int origen) {
+	public void DFS(int origen) {										//Busqueda por profundidad (Recursivo)
 		NodoGrafo vertice = new NodoGrafo();
 		ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
 		
 		vertice = encontrarNodo(origen);
-		aristas = calcularAristas(vertice);
-		aristas.forEach((n)->{
-			DFS(n.apunta.valor);
+		aristas = calcularAristas(vertice);								//Calculo todas las aristas del vertice
+		
+		aristas.forEach((n)->{											//Por cada aristas del vertice
+			DFS(n.apunta.valor);										//Funcion recursiva que va recorrer el array hasta que se vacie
 		});
-		System.out.println(vertice.valor);
+		System.out.println(vertice.valor);								//En la salida del array voy imprimiendo, al ser recursiva la funcion se imprimiran de hijo a padre
 	}
 	
-	public void BFS(int origen) {
+	public void BFS(int origen) {										//Busqueda por anchura (Iterativo)
 		ArrayList<NodoGrafo> vertices = new ArrayList<NodoGrafo>();
 		
-		vertices.add(encontrarNodo(origen));
-		while (!vertices.isEmpty()){
+		vertices.add(encontrarNodo(origen));							//Agrego a mi array el vertice inicial
+		
+		while (!vertices.isEmpty()){					
 			NodoGrafo aux = new NodoGrafo();
 			ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
 			aux = vertices.get(0);
 			vertices.remove(0);
-			aristas = calcularAristas(aux);
+			aristas = calcularAristas(aux);								//Utilizo una función para calcular todos las aristas del vertice 
 			
-			aristas.forEach((n)->{	
+			aristas.forEach((n)->{										//Con todas las aristas del vertice obtengo todos a los que se conecta el mismo 
 				vertices.add(n.apunta);
 			});
-			System.out.println(aux.valor);
+			System.out.println(aux.valor);								//Imprimo una vez que tengo todos los adyacentes
 		}
 	}
 	
@@ -203,67 +205,6 @@ public class GrafoDinamic implements GrafosTDA {
 			aristaAux = aristaAux.sig;
 		}
 		return aristas;
-	}
-	
-	public GrafoDinamic Kruskal() {
-		GrafoDinamic T = new GrafoDinamic();
-		ArrayList<Integer> verticesL = new ArrayList<Integer>();
-		ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
-		ArrayList<ConjuntoTA> componentes = new ArrayList<ConjuntoTA>();
-		verticesL=calcularVertices(this);
-		
-		for (int i = 0; i < verticesL.size(); i++) {	//Agrego todas las aristas a un array de conjuntos
-			ConjuntoTA conjunto = new ConjuntoTA();
-			conjunto.InicializarConjunto();
-			conjunto.Agregar(verticesL.get(i));
-			componentes.add(conjunto);
-			
-			T.agregarVertice(verticesL.get(i));			//Agrego todos los vertices al nuevo grafo T
-		}
-		
-		aristas=calcularAristasTotales();				//Obtengo todas las aristas del grafo
-		Collections.sort(aristas);						//Las ordeno crecientes por peso
-		
-//		aristas.forEach((n)->{							//Verifico que este ordenado
-//			System.out.println("Origen: " + n.origen + " Peso: " + n.peso + " Destino: " + n.apunta.valor);
-//		});
-
-		while(componentes.size()>1) {					//Mientras exista mas de un conjunto...
-			ArrayList<ConjuntoTA> componentesAux = new ArrayList<ConjuntoTA>();
-			ConjuntoTA conjuntoOrigen = new ConjuntoTA();
-			ConjuntoTA conjuntoDestino = new ConjuntoTA();
-			NodoArista auxArista = new NodoArista();
-			int verticeOrigen=0, verticeDestino=0;
-			conjuntoOrigen.InicializarConjunto();
-			conjuntoDestino.InicializarConjunto();
-			auxArista = aristas.get(0);
-			aristas.remove(0);
-			for (int i = 0; i < componentes.size(); i++) {	//Obtengo las posiciones de origen y destino
-				if (componentes.get(i).Pertenece(auxArista.origen)){
-					verticeOrigen = i;
-					conjuntoOrigen = componentes.get(i);
-				}
-				if (componentes.get(i).Pertenece(auxArista.apunta.valor)){
-					verticeDestino = i;
-					conjuntoDestino = componentes.get(i);
-				}
-			}
-			
-			if(verticeOrigen!=verticeDestino) { 		//¿Origen y destino pertenecen a conjuntos diferentes?
-				componentes.remove(conjuntoOrigen);
-				componentes.remove(conjuntoDestino);
-				while (!conjuntoDestino.ConjuntoVacio()) {
-					int elemento = conjuntoDestino.Elegir();
-					conjuntoOrigen.Agregar(elemento);
-					conjuntoDestino.Sacar(elemento);
-				}
-				componentesAux.add(conjuntoOrigen);
-				componentesAux.addAll(componentes);
-				componentes = componentesAux;
-				T.agregarArista(auxArista.origen, auxArista.apunta.valor, auxArista.peso);
-			}
-		}
-		return T;
 	}
 	
 	private ArrayList<Integer> calcularVertices(GrafoDinamic g) {	//Metodo para la obtención de todos los vertices de un grafo
@@ -281,14 +222,117 @@ public class GrafoDinamic implements GrafosTDA {
 		ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
 		ArrayList<Integer> verticesL = new ArrayList<Integer>();
 		verticesL=calcularVertices(this);
-		verticesL.forEach((n)->{
+		
+		for (int i = 0; i < verticesL.size(); i++) {
 			NodoArista aristaAux = new NodoArista();
-			aristaAux = encontrarNodo(n).lista;
+			aristaAux = encontrarNodo(verticesL.get(i)).lista;
 			while (aristaAux != null) {
 				aristas.add(aristaAux);
 				aristaAux = aristaAux.sig;
 			}
-		});
+		}
 		return aristas;
+	}
+	
+	public GrafoDinamic Kruskal() {
+		GrafoDinamic T = new GrafoDinamic();						//Grafo resultado
+		ArrayList<Integer> verticesL = new ArrayList<Integer>();
+		ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
+		ArrayList<ConjuntoTA> componentes = new ArrayList<ConjuntoTA>();
+		
+		verticesL=calcularVertices(this);							//Obtengo todos los vertices del grafo
+		for (int i = 0; i < verticesL.size(); i++) {				//Agrego todas las aristas a un array de conjuntos
+			ConjuntoTA conjunto = new ConjuntoTA();
+			conjunto.InicializarConjunto();
+			conjunto.Agregar(verticesL.get(i));
+			componentes.add(conjunto);
+			
+			T.agregarVertice(verticesL.get(i));						//Agrego todos los vertices al nuevo grafo T
+		}
+		aristas=calcularAristasTotales();							//Obtengo todas las aristas del grafo
+		Collections.sort(aristas);									//Las ordeno crecientes por peso
+		while(componentes.size()>1) {								//Mientras exista mas de un conjunto...
+			ArrayList<ConjuntoTA> componentesAux = new ArrayList<ConjuntoTA>();
+			ConjuntoTA conjuntoOrigen = new ConjuntoTA();
+			ConjuntoTA conjuntoDestino = new ConjuntoTA();
+			NodoArista auxArista = new NodoArista();
+			int verticeOrigen=0, verticeDestino=0;
+			conjuntoOrigen.InicializarConjunto();
+			conjuntoDestino.InicializarConjunto();
+			auxArista = aristas.get(0);
+			aristas.remove(0);
+			for (int i = 0; i < componentes.size(); i++) {			//Obtengo las posiciones de origen y destino
+				if (componentes.get(i).Pertenece(auxArista.origen)){
+					verticeOrigen = i;
+					conjuntoOrigen = componentes.get(i);
+				}
+				if (componentes.get(i).Pertenece(auxArista.apunta.valor)){
+					verticeDestino = i;
+					conjuntoDestino = componentes.get(i);
+				}
+			}
+			if(verticeOrigen!=verticeDestino) { 					//¿Origen y destino pertenecen a conjuntos diferentes?
+				componentes.remove(conjuntoOrigen);
+				componentes.remove(conjuntoDestino);
+				while (!conjuntoDestino.ConjuntoVacio()) {
+					int elemento = conjuntoDestino.Elegir();
+					conjuntoOrigen.Agregar(elemento);
+					conjuntoDestino.Sacar(elemento);
+				}
+				componentesAux.add(conjuntoOrigen);
+				componentesAux.addAll(componentes);
+				componentes = componentesAux;
+				T.agregarArista(auxArista.origen, auxArista.apunta.valor, auxArista.peso);
+			}
+		}
+		return T;
+	}
+	
+	public GrafoDinamic Prim() {
+		GrafoDinamic T = new GrafoDinamic();										//Grafo resultado
+		ArrayList<Integer> verticesL = new ArrayList<Integer>();					//Array de vertices
+		ArrayList<Integer> verticesRecorridos = new ArrayList<Integer>();			//Array de vertices recorridos
+		ArrayList<NodoArista> aristasL = new ArrayList<NodoArista>();				//Array de aristas
+		
+		verticesL=calcularVertices(this);											//Obtengo todos los vertices del grafo
+		Collections.sort(aristasL);													//Las ordeno crecientes
+		verticesRecorridos.add(this.nodo.valor);
+		
+		while(verticesL.size() != verticesRecorridos.size()) { 						//Itero mientras que los vertices recorridos sean todos los vertices del grafo
+			NodoArista arista = GetMenorAdj(verticesRecorridos, verticesL);			//Obtengo la menor arista entre de la disyunción
+			T.agregarVertice(arista.origen);
+			T.agregarVertice(arista.apunta.valor);
+			T.agregarArista(arista.origen, arista.apunta.valor, arista.peso);
+			verticesRecorridos.add(arista.apunta.valor);							//Agrego al vertice recorrido el nodo destino
+		}
+		return T;
+	}
+	
+	public NodoArista GetMenorAdj(ArrayList<Integer> s, ArrayList<Integer> destinos){
+		ArrayList<Integer> auxDiferencia = new ArrayList<Integer>();
+		destinos.forEach((n)->{													//Guardo los valores del array destino, todos los vertices.
+			auxDiferencia.add(n);
+		});
+		
+		for(int i=0; i< s.size(); i++){											//Hago la diferencia entre los arreglos
+			if(auxDiferencia.contains(s.get(i))){
+				auxDiferencia.remove(s.get(i));
+			}
+		}
+		ArrayList<NodoArista> aristas = new ArrayList<NodoArista>();
+		for(int i=0; i< s.size(); i++){
+			NodoGrafo auxVertice = encontrarNodo(s.get(i));				//Agarro el primer vertice para buscar sus aristas
+			NodoArista auxArista = auxVertice.lista;
+			while (auxArista != null) {									//Recorro las aristas
+				if(auxDiferencia.contains(auxArista.apunta.valor)) {	//Si la arista del primer nodo origen tiene como destino uno de los nodos de la disyuncion, la agrego al arreglo 
+					auxArista.origen = auxVertice.valor;
+					aristas.add(auxArista);
+				}	
+				auxArista = auxArista.sig;
+			}
+		}
+
+		Collections.sort(aristas);							// Ya tengo todas las aristas con origgen en vertices origen y destinos en la disyuncion
+		return aristas.get(0);								//Devuelvo la menor arista
 	}
 }
--- src/run/Main.java
+++ src/run/Main.java
@@ -35,31 +35,35 @@ public class Main {
 		grafoK.agregarVertice(6);
 		
 		grafoK.agregarArista(1, 2, 6);
+		grafoK.agregarArista(2, 1, 6);
 		grafoK.agregarArista(1, 3, 1);
+		grafoK.agregarArista(3, 1, 1);
 		grafoK.agregarArista(1, 4, 5);
+		grafoK.agregarArista(4, 1, 5);
 		grafoK.agregarArista(2, 3, 5);
+		grafoK.agregarArista(3, 2, 5);
 		grafoK.agregarArista(2, 5, 3);
+		grafoK.agregarArista(5, 2, 3);
 		grafoK.agregarArista(3, 4, 5);
+		grafoK.agregarArista(4, 3, 5);
 		grafoK.agregarArista(3, 5, 6);
+		grafoK.agregarArista(5, 3, 6);
 		grafoK.agregarArista(3, 6, 4);
+		grafoK.agregarArista(6, 3, 4);
 		grafoK.agregarArista(4, 6, 2);
+		grafoK.agregarArista(6, 4, 2);
 		grafoK.agregarArista(5, 6, 6);
+		grafoK.agregarArista(6, 5, 6);
 		
-//		grafo.mostrarMatriz();
+		
 		System.out.println("Depth-First-Search");
-//		System.out.println("Grafo");
-		grafo.DFS(1);
-//		System.out.println("GrafoK");
-//		grafoK.DFS(1);
-//		
+		grafo.DFS(1);		
 		System.out.println("Breath-First-Search");
-//		System.out.println("Grafo");
 		grafo.BFS(1);
-//		System.out.println("GrafoK");
-//		grafoK.BFS(1);
 		System.out.println("Kruskal");
-		grafoK = grafoK.Kruskal();
-		grafoK.mostrarMatriz();
+		grafoK.Kruskal().mostrarMatriz();
+		System.out.println("Prim");
+		grafoK.Prim().mostrarMatriz();
 	}
 
 }
